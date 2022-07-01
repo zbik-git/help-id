e499fa976c4ba2c32f4e5c113aa7203d956be870
Michał Żbik <m.zbik@op.pl>
2022-07-01 21:01:57 +0200

    add articles table

--- pages/articles/showArticles.vue
+++ pages/articles/showArticles.vue
@@ -2,7 +2,51 @@
 <div>
     <div class="max-w-md mx-auto pt-32">
       
-    {{ articles }}
+    
+
+
+    <div v-if="articles">
+        <table class="colapse border-blue-500 [&>*]:border-2 [&>*]:p-2  " >
+           <thead >
+                <tr class="[&>*]:border-2 [&>*]:p-2 bg-slate-400">
+                    <th >
+                    id
+                   </th>
+                    <th >
+                    title
+                   </th>
+                    <th >
+                    content
+                   </th>
+                    <th >
+                    category
+                   </th>
+                </tr>
+
+            </thead>
+            <tbody v-for="article in articles.data.articlesList " :key='article.id'>
+                <tr class="[&>*]:border-2 [&>*]:p-2">
+                    <td>
+                        {{  article.id }}
+                    </td>
+                   
+                    <td>
+                        {{ article.title }}
+
+                    </td>
+                    <td>
+                        {{ article.content }}
+                    </td>
+                    <td>
+
+                        {{ article.category }}
+                    </td>
+                </tr>
+
+                
+            </tbody>
+        </table>
+    </div>
     
     </div>
 </div>
